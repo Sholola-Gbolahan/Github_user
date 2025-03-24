4f059fd1666158bf8f3ce5f52a64d4b76920c2df
Gbolahan <shololagbolahan@gmail.com>
2025-03-24 04:53:22 +0100

    15.2 forks setup done and data added to chart for stars and forks

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -48,6 +48,8 @@ const Repos = () => {
       const { stargazers_count, name, forks } = item
 
       total.stars[stargazers_count] = { label: name, value: stargazers_count }
+      total.forks[forks] = { label: name, value: forks }
+
       return total
     },
     {
@@ -55,12 +57,9 @@ const Repos = () => {
       forks: {},
     }
   )
-
-  console.log(stars)
-  // converting stars as an object to array
   stars = Object.values(stars).slice(-5).reverse()
+  forks = Object.values(forks).slice(-5).reverse()
 
-  console.log(stars)
   const chartData = [
     {
       label: "HTML",
@@ -80,9 +79,9 @@ const Repos = () => {
     <section className="section">
       <Wrapper className="section-center">
         <Pie3D data={mostUsed} />
-        <Column3D data={chartData} />
+        <Column3D data={stars} />
         <Doughnut2D data={mostPopular} />
-        <Bar3D data={chartData} />
+        <Bar3D data={forks} />
       </Wrapper>
     </section>
   )
