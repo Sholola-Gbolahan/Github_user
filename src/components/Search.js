183fb8999b828524f75c8cc8ede20e8468039b3b
Gbolahan <shololagbolahan@gmail.com>
2025-03-26 09:01:15 +0100

    16.3 Error handling func. setup done to display error when limit is zero

--- src/components/Search.js
+++ src/components/Search.js
@@ -6,7 +6,7 @@ const Search = () => {
   const [user, setUser] = React.useState("")
 
   // get things from global context
-  const { requests } = React.useContext(GithubContext)
+  const { requests, error } = React.useContext(GithubContext)
 
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -16,6 +16,11 @@ const Search = () => {
   return (
     <section className="section">
       <Wrapper className="section-center">
+        {error.show && (
+          <ErrorWrapper>
+            <p>{error.msg}</p>
+          </ErrorWrapper>
+        )}
         <form onSubmit={handleSubmit}>
           <div className="form-control">
             <MdSearch />
--- src/context/context.js
+++ src/context/context.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, createContext } from "react"
+import React, { useState, useEffect, Context } from "react"
 import mockUser from "./mockData.js/mockUser"
 import mockRepos from "./mockData.js/mockRepos"
 import mockFollowers from "./mockData.js/mockFollowers"
@@ -17,25 +17,38 @@ const GithubProvider = ({ children }) => {
 
   const [requests, setRequest] = React.useState("")
   const [loading, SetLoading] = React.useState(false)
+  const [error, setError] = React.useState({ show: false, msg: "" })
 
+  // Check Rate
   const checkRequest = () => {
     axios(`${rootUrl}/rate_limit`)
       .then(({ data }) => {
         let {
           rate: { remaining },
         } = data
+
         setRequest(remaining)
+
         if (remaining === 0) {
           // throw error
+          toggleError(true, "you have exceeded your search limit")
         }
       })
       .catch((err) => console.log(err))
   }
 
-  useEffect(checkRequest(), [])
+  function toggleError(show, msg) {
+    setError({ show, msg })
+  }
+
+  useEffect(() => {
+    checkRequest()
+  }, [])
 
   return (
-    <GithubContext.Provider value={{ repos, githubUser, followers, requests }}>
+    <GithubContext.Provider
+      value={{ repos, githubUser, followers, requests, error }}
+    >
       {children}
     </GithubContext.Provider>
   )
